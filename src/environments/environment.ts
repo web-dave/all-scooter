036703e6ebc708561d1604ba8e1eeb138e8777ec
David Müllerchen <dave@webdave.de>
2026-04-23 14:00:31 +0200

    Replace placeholder gKey with imported value

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
+import { gKey } from './../../keys';
 export const environment = {
-  gKey: '---',
+  gKey: gKey,
   dottUrl: 'https://gbfs.api.ridedott.com/public/v2/',
   limeUrl: 'https://data.lime.bike/api/partners/v1/gbfs/',
 };
