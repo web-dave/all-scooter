1aeb1b3a00de8dd7e4800ee5cf95b224e4d7e4c0
Dave <dave@webdave.de>
2026-04-27 21:51:52 +0200

    lime php proxy Works

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,6 +2,6 @@ import { gKey } from './../../keys';
 export const environment = {
   gKey: gKey,
   dottUrl: 'https://gbfs.api.ridedott.com/public/v2/',
-  limeUrl: 'limeapi/',
-  limeUsePhpProxy: false,
+  limeUrl: 'https://all-scooter.webdave.rocks/lime.php',
+  limeUsePhpProxy: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { gKey } from './../../keys';
 export const environment = {
   gKey: gKey,
   dottUrl: 'https://gbfs.api.ridedott.com/public/v2/',
-  limeUrl: 'lime.php',
+  limeUrl: 'https://all-scooter.webdave.rocks/lime.php',
   limeUsePhpProxy: true,
 };
